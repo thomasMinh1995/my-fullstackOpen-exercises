7ce3b6ca05672fd00e7acb7fac1685161298f36b
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-03 22:16:17 +0700

    Sep-03-2024: conditional rendering statistic

--- src/Part1.jsx
+++ src/Part1.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+
+const Statistics = (props) => {
+  if(props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <p>No stratitic</p>
+    )
+  }
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+      <p>All {props.all} </p>
+      <p>Average {props.average}</p>
+    </div>
+  );
+};
 
 export default function Part1() {
-  
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-    const all = good + neutral + bad;
-    const average = all / 3;
+  const all = good + neutral + bad;
+  const average = all / 3;
   return (
     <>
       <h1>Give feedback</h1>
@@ -16,14 +33,7 @@ export default function Part1() {
         <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
         <button onClick={() => setBad(bad + 1)}>Bad</button>
       </div>
-      <div>
-        <h1>Statistics</h1>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
-        <p>All {all} </p>
-        <p>Average {average}</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average}/>
     </>
-  )
+  );
 }
