65c983ff44b6cfbb468e6554bf1cb7030624edfd
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-03 22:44:27 +0700

    Sep-03-2024: passing statechild component button statistic

--- src/Part1.jsx
+++ src/Part1.jsx
@@ -1,5 +1,21 @@
 import React, { useState } from "react";
 
+const StatisticLine  = (props) => {
+  return (
+    <>
+      <p>{props.text} {props.value}</p>
+    </>
+  )
+}
+
+const Button = (props) => {
+  return(
+    <>
+      <button onClick={props.onClick}>{props.text}</button>
+    </>
+  )
+}
+
 const Statistics = (props) => {
   if(props.good === 0 && props.neutral === 0 && props.bad === 0) {
     return (
@@ -9,11 +25,11 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>Good {props.good}</p>
-      <p>Neutral {props.neutral}</p>
-      <p>Bad {props.bad}</p>
-      <p>All {props.all} </p>
-      <p>Average {props.average}</p>
+      <StatisticLine text="good" value={props.good}/>
+      <StatisticLine text="neutral" value={props.neutral}/>
+      <StatisticLine text="bad" value={props.bad}/>
+      <StatisticLine text="all" value={props.all}/>
+      <StatisticLine text="average" value={props.average}/>
     </div>
   );
 };
@@ -23,15 +39,19 @@ export default function Part1() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleGoodValue = () => setGood(good + 1);
+  const handleNeutralValue = () => setNeutral(neutral + 1);
+  const handleBadlValue = () => setBad(bad + 1);
+
   const all = good + neutral + bad;
   const average = all / 3;
   return (
     <>
       <h1>Give feedback</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>Good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-        <button onClick={() => setBad(bad + 1)}>Bad</button>
+        <Button text="good" onClick={handleGoodValue} />
+        <Button text="neutral" onClick={handleNeutralValue} />
+        <Button text="bad" onClick={handleBadlValue} />
       </div>
       <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average}/>
     </>
