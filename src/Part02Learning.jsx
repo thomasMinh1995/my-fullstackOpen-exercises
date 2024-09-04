7981c4e086c75ac3d4b7e3c7a65a7ad827682474
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-04 16:40:49 +0700

    Sep-04-2024: create add notes learning part 02

--- src/Part02Learning.jsx
+++ src/Part02Learning.jsx
@@ -1,13 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Note = (prop) => {
-  return (
-    <p>{prop.content}</p>
-  )
-}
+  return <p>{prop.content}</p>;
+};
 
 export default function Part02Learning() {
-  const notes = [
+  const [notes, setNotes] = useState([
     {
       id: 1,
       content: "HTML is easy",
@@ -23,13 +21,35 @@ export default function Part02Learning() {
       content: "GET and POST are the most important methods of HTTP protocol",
       important: true,
     },
-  ];
+  ]);
+
+  const [newNote, setNewNote] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const noteObj = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+    setNotes(notes.concat(noteObj));
+    setNewNote('')
+  }
+
+  const handleChangeNewNote = (event) => {
+    setNewNote(event.target.value)
+    console.log('note new', newNote);
+  }
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="add note" value={newNote} onChange={handleChangeNewNote}/>
+        <button>Save</button>
+      </form>
       {notes.map((note) => (
         // <p key={note.id}>{note.content}</p>
-        <Note key={note.id} content={note.content}/>
+        <Note key={note.id} content={note.content} />
       ))}
     </div>
   );
