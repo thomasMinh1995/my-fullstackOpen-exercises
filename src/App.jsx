016fbb88b3674a572b40e6ad9ce94ed31c1e8cd2
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-04 09:20:51 +0700

    Sep-04-2024: add vote

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import Part1 from './Part1'
+// import { useState } from 'react'
+// import Part1 from './Part1'
+
+import Anecdotes from "./Anecdotes"
 
 function App() {
 
   return (
     <>
       <h1>Hello</h1>
-      <Part1/>
+      {/* <Part1/> */}
+      <Anecdotes /> 
     </>
   )
 }
