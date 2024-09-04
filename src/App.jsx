641c2c016c76eabb8190dafa0f55ab04d0dfdd49
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-04 09:30:03 +0700

    Sep-04-2024: update code part 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // import { useState } from 'react'
 // import Part1 from './Part1'
 
-import Anecdotes from "./Anecdotes"
+// import Anecdotes from "./Anecdotes"
 
 function App() {
 
   return (
     <>
       <h1>Hello</h1>
+      {/* Part01 */}
       {/* <Part1/> */}
-      <Anecdotes /> 
+      {/* <Anecdotes />  */}
+
     </>
   )
 }
